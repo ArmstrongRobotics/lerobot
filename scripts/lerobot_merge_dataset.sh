09fa4ecf1b3b919da3c7e0cd5cda9e0a4425e028
Alvin <alvin@armstrong.ai>
2025-12-30 11:31:20 -0800

    merge

--- scripts/lerobot_merge_dataset.sh
+++ scripts/lerobot_merge_dataset.sh
@@ -1,4 +1,9 @@
+# lerobot-edit-dataset  \
+#     --repo_id ${HF_USER}/pnp_cup_merged_basic123_random_start \
+#     --operation.type merge \
+#     --operation.repo_ids "['${HF_USER}/pick_and_place_single_cup_in_tub_basic', '${HF_USER}/pick_and_place_single_cup_in_tub_basic2', '${HF_USER}/pnp_cup_basic3', '${HF_USER}/pnp_cup_random_start']"
+
 lerobot-edit-dataset  \
-    --repo_id ${HF_USER}/pnp_cup_merged_basic123_random_start \
+    --repo_id ${HF_USER}/pnp_9in_plates_recover_finger_merged \
     --operation.type merge \
-    --operation.repo_ids "['${HF_USER}/pick_and_place_single_cup_in_tub_basic', '${HF_USER}/pick_and_place_single_cup_in_tub_basic2', '${HF_USER}/pnp_cup_basic3', '${HF_USER}/pnp_cup_random_start']"
+    --operation.repo_ids "['${HF_USER}/pnp_9in_plates_corner', '${HF_USER}/pnp_9in_plates_corner_recover', '${HF_USER}/pnp_9in_plates_corner_recover_finger_sep']"
